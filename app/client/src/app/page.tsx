acbb1027b8c188b01cc664195e29760ee0921c40
EvannPopoff <evann.popoff@gmail.com>
2026-03-05 20:23:22 +0100

    Configuration NextJS & structuration dossiers / fichiers

---
+++ app/client/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold"> test 8 Bienvenue sur l'application Next.js !</h1>
+    </main>
+  );
+}
