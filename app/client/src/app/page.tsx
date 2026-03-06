a49d16bf62a450322763406ad89dd4c272320b7c
EvannPopoff <evann.popoff@gmail.com>
2026-03-06 15:20:11 +0100

    Home base

--- app/client/src/app/page.tsx
+++ app/client/src/app/page.tsx
@@ -1,7 +1,56 @@
-export default function Home() {
+"use client";
+import Button from '@/components/ui/button/button';
+import { Monitor, Smartphone, Settings } from 'lucide-react';
+import styles from './page.module.scss';
+
+export default function HomePage() {
+  const actions = [
+    {
+      title: "Borne d'Accueil",
+      desc: "Inscription des visiteurs et QR Codes",
+      subtitle: "Interface borne tactile",
+      icon: Monitor
+    },
+    {
+      title: "WebApp Accompagnateur",
+      desc: "Prise en charge et suivi des visiteurs",
+      subtitle: "Mobile / Tablette",
+      icon: Smartphone
+    },
+    {
+      title: "Administration",
+      desc: "Statistiques, immersions et utilisateurs",
+      subtitle: "Desktop",
+      icon: Settings
+    },
+  ];
+
   return (
-    <main className="flex items-center justify-center min-h-screen">
-      <h1 className="text-3xl font-bold"> test 8 Bienvenue sur l'application Next.js !</h1>
-    </main>
+    <div className={styles.mainContainer}>
+      <header className={styles.header}>
+        <div className={styles.logoPlaceholder}>
+          <img src="/assets/images/logo_uca.png" alt="Logo UCA" />
+        </div>
+      </header>
+
+      <main className="container">
+        <section className={styles.heroSection}>
+          <h1 className={styles.mainTitle}>Journées Portes Ouvertes</h1>
+          <p className={styles.mainSubtitle}>IUT Clermont Auvergne - Université Clermont Auvergne - Réalisé par les étudiants de l'IUT du Puy-en-Velay.</p>
+        </section>
+
+        <div className={styles.buttonGrid}>
+          {actions.map((item, index) => (
+            <Button
+              key={index}
+              title={item.title}
+              description={item.desc}
+              subtitle={item.subtitle}
+              Icon={item.icon}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
   );
 }
--- app/client/src/components/layout/footer/footer.tsx
+++ app/client/src/components/layout/footer/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div className={styles.container}>
         <p className={styles.text}>
-          © 2026 IUT Clermont Auvergne — Université Clermont Auvergne
+          © 2026 IUT Clermont Auvergne - Université Clermont Auvergne - Réalisé par les étudiants de l'IUT du Puy-en-Velay.
         </p>
       </div>
     </footer>
